40f3e44c444b34fe4b19af2e2127b5ec7e431655
matt <matt17@qq.com>
2021-12-15 02:04:52 +0800

    init

---
+++ src/main/java/com/matt/mapreduce/Main.java
@@ -0,0 +1,14 @@
+package com.matt.mapreduce;
+
+/**
+ * @author matt
+ * @create 2021-12-15 1:26
+ */
+public class Main {
+
+
+    public void add(int i, int j) {
+
+    }
+
+}
---
+++ src/main/java/com/matt/mapreduce/wordcount/WordCountDriver.java
@@ -0,0 +1,11 @@
+package com.matt.mapreduce.wordcount;
+
+/**
+ * @author matt
+ * @create 2021-12-15 1:29
+ */
+public class WordCountDriver {
+
+
+
+}
